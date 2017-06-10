35a41085f394616fa69709696d7457c3da43cd23
Michael Pankov <work@michaelpankov.com>
2017-06-10 16:51:57 +0300

    Уносим в модули

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,49 @@
-// прямоугольник
-struct Rectangle {
-    // ширина
-    width: f64,
-    // длина
-    length: f64,
-}
+mod figures {
+    pub struct Rectangle {
+        width: f64,
+        length: f64,
+    }
 
-// квадрат
-struct Square {
-    // сторона
-    side: f64,
-}
+    pub struct Square {
+        side: f64,
+    }
 
-impl Rectangle {
-    fn new(width: f64, length: f64) -> Option<Rectangle> {
-        if width > 0. && length > 0. {
-            Some( Rectangle { length, width } )
-        } else {
-            None
+    impl Rectangle {
+        pub fn new(width: f64, length: f64) -> Option<Rectangle> {
+            if width > 0. && length > 0. {
+                Some( Rectangle { length, width } )
+            } else {
+                None
+            }
+        }
+        pub fn area(&self) -> f64
+        {
+            self.width * self.length
         }
     }
-    fn area(&self) -> f64
-    {
-        self.width * self.length
+
+    impl Square {
+        pub fn new(side: f64) -> Option<Square> {
+            if side > 0. {
+                Some( Square { side } )
+            } else {
+                None
+            }
+        }
+        pub fn area(&self) -> f64
+        {
+            self.side * self.side
+        }
     }
 }
 
-impl Square {
-    fn new(side: f64) -> Option<Square> {
-        if side > 0. {
-            Some( Square { side } )
-        } else {
-            None
-        }
-    }
-    fn area(&self) -> f64
-    {
-        self.side * self.side
-    }
-}
 
 fn main() {
-    let rect1 = Rectangle::new(3., 5.).unwrap();
-    let rect2 = Rectangle::new(4., 6.).unwrap();;
-    let rect3 = Rectangle::new(-4., 6.).unwrap();;
+    let rect1 = figures::Rectangle::new(3., 5.).unwrap();
+    let rect2 = figures::Rectangle::new(4., 6.).unwrap();;
 
-    let sq1 = Square::new(8.).unwrap();
-    let sq2 = Square::new(4.).unwrap();
+    let sq1 = figures::Square::new(8.).unwrap();
+    let sq2 = figures::Square::new(4.).unwrap();
 
     let rects = [&rect1, &rect2];
     let squares = [&sq1, &sq2];

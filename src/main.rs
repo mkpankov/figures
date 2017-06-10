0a2f6d8da733c10465bd2280a8947c97e8117df9
Michael Pankov <work@michaelpankov.com>
2017-06-10 18:06:01 +0300

    Используем типажи

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,67 @@
+use figures::{Area, Rectangle, Square};
+
 mod figures {
+    pub trait Area {
+        fn area(&self) -> f64;
+    }
+
     pub struct Rectangle {
         width: f64,
         length: f64,
     }
 
     pub struct Square {
-        side: f64,
+        rectangle: Rectangle,
     }
 
     impl Rectangle {
-        pub fn new(width: f64, length: f64) -> Option<Rectangle> {
+        pub fn new(width: f64, length: f64) -> Option<Rectangle>
+        {
             if width > 0. && length > 0. {
                 Some( Rectangle { length, width } )
             } else {
                 None
             }
         }
-        pub fn area(&self) -> f64
+    }
+
+    impl Area for Rectangle {
+        fn area(&self) -> f64
         {
             self.width * self.length
         }
     }
 
     impl Square {
-        pub fn new(side: f64) -> Option<Square> {
+        pub fn new(side: f64) -> Option<Square>
+        {
             if side > 0. {
-                Some( Square { side } )
+                Some( Square { rectangle: Rectangle { length: side, width: side } } )
             } else {
                 None
             }
         }
-        pub fn area(&self) -> f64
+    }
+
+    impl Area for Square {
+        fn area(&self) -> f64
         {
-            self.side * self.side
+            self.rectangle.area()
         }
     }
 }
 
 
 fn main() {
-    let rect1 = figures::Rectangle::new(3., 5.).unwrap();
-    let rect2 = figures::Rectangle::new(4., 6.).unwrap();;
+    let rect1 = Rectangle::new(3., 5.).unwrap();
+    let rect2 = Rectangle::new(4., 6.).unwrap();
 
-    let sq1 = figures::Square::new(8.).unwrap();
-    let sq2 = figures::Square::new(4.).unwrap();
+    let sq1 = Square::new(8.).unwrap();
+    let sq2 = Square::new(4.).unwrap();
 
-    let rects = [&rect1, &rect2];
-    let squares = [&sq1, &sq2];
+    let figures_with_area: [&Area; 4] = [&rect1, &rect2, &sq1, &sq2];
 
-    for r in rects.iter() {
-        println!("Площадь равна {}", r.area());
-    }
-
-    for s in squares.iter() {
-        println!("Площадь равна {}", s.area());
+    for f in figures_with_area.iter() {
+        println!("Площадь равна {}", f.area());
     }
 }
